b7b2ea8938872bbc64001bff22bd26505d8ceb13
mcdott <111994803+mcdott@users.noreply.github.com>
2023-02-16 13:46:06 -0500

    Added dynamic display of the tip amount and the total amount

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,37 +3,45 @@ import { useState } from "react";
 import "./Calculator.css";
 
 function Calculator() {
-  const [bill, setBill] = useState(0);
-  const [tip, setTip] = useState(0);
-  const [people, setPeople] = useState(0);
+  const [bill, setBill] = useState(100);
+  const [tip_percent, setTipPercent] = useState(15);
+  const [people, setPeople] = useState(1);
 
   return (
     <div className='Calculator'>
       <h1>Tip Calculator</h1>
       <form>
         <div className='bill'>
-          <label>Bill: $ </label>
+          <label>Bill $ </label>
           <input
             type='text'
             value={bill}
             onChange={(e) => setBill(e.target.value)}
           />
         </div>
-        <div className='tip'>
-          <label>Tip: % </label>
+        <div className='tip-percent'>
+          <label>Tip % </label>
           <input
             type='text'
-            value={tip}
-            onChange={(e) => setTip(e.target.value)}
+            value={tip_percent}
+            onChange={(e) => setTipPercent(e.target.value)}
           />
         </div>
         <div className='people'>
-          <label>People: </label>
+          <label>Number of People: </label>
           <input
             type='text'
             value={people}
             onChange={(e) => setPeople(e.target.value)}
           />
+        </div>
+        <div className='tip-amount'>
+          <h2> Tip </h2>
+          <p>{((bill * (tip_percent / 100)) / people).toFixed(2)}</p>
+        </div>
+        <div className='total'>
+          <h2> Total </h2>
+          <p>{((bill * (1 + tip_percent / 100)) / people).toFixed(2)}</p>
         </div>
       </form>
     </div>
